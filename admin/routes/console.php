72b17757b9cd860e93ea361e4e27046961edad97
Imam Ali Mustofa <d.darkterminal@gmail.com>
2025-03-13 17:42:31 +0700

    refactor: change stats fetcher running hourly

--- admin/routes/console.php
+++ admin/routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:stats-fetcher')->everyThirtyMinutes();
+Schedule::command('app:stats-fetcher')->hourly();
