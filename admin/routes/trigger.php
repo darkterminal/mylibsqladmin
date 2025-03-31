95c26a5ae9848c592ed6f5202d0be2fbfc962f07
Imam Ali Mustofa <d.darkterminal@gmail.com>
2025-04-01 02:25:49 +0700

    feat(activities): add store endpoint for activity logs

--- admin/routes/trigger.php
+++ admin/routes/trigger.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Events\TriggerDatabaseStatsChangeEvent;
+use App\Http\Controllers\ActivityLogController;
 
 Route::middleware('auth')->group(function () {
     Route::get('/trigger-stats-changed/{databaseName?}', function ($databaseName) {
         event(new TriggerDatabaseStatsChangeEvent($databaseName));
         return "Stats changed for {$databaseName}";
     })->name('trigger.stats-changed');
+    Route::post('/activities', [ActivityLogController::class, 'store'])->name('activities.store');
 });
