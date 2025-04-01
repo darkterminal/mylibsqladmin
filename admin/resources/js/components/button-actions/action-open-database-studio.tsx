a3d7676a9fb875d08a26bb648344bb77bffe4054
Imam Ali Mustofa <d.darkterminal@gmail.com>
2025-04-01 12:49:44 +0700

    feat(button): make size configurable in database studio action

--- admin/resources/js/components/button-actions/action-open-database-studio.tsx
+++ admin/resources/js/components/button-actions/action-open-database-studio.tsx
@@ -1,14 +1,15 @@
 import { router } from "@inertiajs/react";
+import { VariantProps } from "class-variance-authority";
 import { BoxIcon } from "lucide-react";
 import { AppTooltip } from "../app-tooltip";
 import { Button } from "../ui/button";
 
-export default function ButtonOpenDatabaseStudio({ databaseName }: { databaseName: string }) {
+export default function ButtonOpenDatabaseStudio({ databaseName, size = "sm" }: { databaseName: string, size?: VariantProps<typeof Button>["size"] }) {
     return (
         <AppTooltip text="Open Database Studio">
             <Button
                 variant="outline"
-                size="sm"
+                size={size}
                 onClick={() => router.get(route('dashboard'), { database: databaseName })}
             >
                 <BoxIcon className="h-2 w-2" />
