ce63c358277c35342e63ceeb987a5be391c736c9
Imam Ali Mustofa <d.darkterminal@gmail.com>
2025-04-02 17:47:26 +0700

    refactor: database access, add local storage handling, and improve team/database navigation

--- admin/resources/js/components/button-actions/action-open-database-studio.tsx
+++ admin/resources/js/components/button-actions/action-open-database-studio.tsx
@@ -1,16 +1,39 @@
+import { apiFetch } from "@/lib/api";
+import { Team } from "@/types";
 import { router } from "@inertiajs/react";
 import { VariantProps } from "class-variance-authority";
 import { BoxIcon } from "lucide-react";
 import { AppTooltip } from "../app-tooltip";
 import { Button } from "../ui/button";
 
-export default function ButtonOpenDatabaseStudio({ databaseName, size = "sm" }: { databaseName: string, size?: VariantProps<typeof Button>["size"] }) {
+export default function ButtonOpenDatabaseStudio({
+    databaseName,
+    team,
+    size = "sm"
+}: {
+    databaseName: string,
+    team?: Team,
+    size?: VariantProps<typeof Button>["size"]
+}) {
+    const handleSelectDatabase = async (database: string) => {
+        const currentUrl = window.location.href;
+        localStorage.setItem('sidebar', 'false');
+        localStorage.setItem('prevUrl', currentUrl);
+
+        if (team) {
+            localStorage.setItem('currentTeamId', team.id.toString());
+            await apiFetch(route('api.teams.databases', team.id));
+        }
+
+        router.get(route('database.studio', { database }));
+    }
+
     return (
         <AppTooltip text="Open Database Studio">
             <Button
                 variant="outline"
                 size={size}
-                onClick={() => router.get(route('dashboard'), { database: databaseName })}
+                onClick={() => handleSelectDatabase(databaseName)}
             >
                 <BoxIcon className="h-2 w-2" />
             </Button>
