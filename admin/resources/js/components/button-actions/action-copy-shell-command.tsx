092a48d50942cd3e13cee5cb5e18eca0faa38d02
Eelco <eelcoschreurs@gmail.com>
2025-04-20 22:00:22 +0200

    correct "copy to shell command" with sqldHost and sqldPort

--- admin/resources/js/components/button-actions/action-copy-shell-command.tsx
+++ admin/resources/js/components/button-actions/action-copy-shell-command.tsx
@@ -1,11 +1,15 @@
 import { AppTooltip } from "@/components/app-tooltip";
 import { Button } from "@/components/ui/button";
 import useCopyToClipboard from "@/hooks/copy-to-clipboard";
-import { type UserDatabaseTokenProps } from "@/types";
+import { Configs, SharedData, type UserDatabaseTokenProps } from "@/types";
+import { usePage } from '@inertiajs/react';
 import { CheckIcon, TerminalIcon } from "lucide-react";
 
 export default function ButtonCopyShellCommand({ token }: { token: UserDatabaseTokenProps }) {
+    const { props } = usePage<SharedData>();
     const { copiedText, copyToClipboard } = useCopyToClipboard();
+    const { sqldHost, sqldPort } = props.configs as Configs;
+
     return (
         <AppTooltip text="Copy Shell Command">
             <Button
@@ -14,7 +18,7 @@ export default function ButtonCopyShellCommand({ token }: { token: UserDatabaseT
                 onClick={() => {
                     const protocol = window.location.protocol
                     const hostname = window.location.hostname
-                    const cmd = `turso db shell $(echo "${protocol}//${token.database.database_name}.${hostname}:8080?authToken=${token.full_access_token}")`
+                    const cmd = `turso db shell $(echo "${protocol}//${token.database.database_name}.${sqldHost}:${sqldPort}/?authToken=${token.full_access_token}")`
                     copyToClipboard(cmd, token.name, 'shell')
                 }}
             >
