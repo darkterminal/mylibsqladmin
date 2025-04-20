0c9d3c7db3d3b90c2c6b084e9dc1c2dbd63a66ad
Eelco <eelcoschreurs@gmail.com>
2025-04-20 23:02:41 +0200

    removed migration for is_archived column, updated routes api to check deleted_at == null to set active / inactive

--- admin/routes/api.php
+++ admin/routes/api.php
@@ -25,7 +25,7 @@ Route::middleware('auth')->group(function () {
             $transformed = array_map(function ($db) {
                 return [
                     'name' => $db['database_name'],
-                    'status' => $db['is_archived'] == "1" ? 'active' : 'inactive',
+                    'status' => $db['deleted_at'] != null ? 'inactive' : 'active',
                     'path' => $db['database_name'],
                 ];
             }, $localDbs);
