e5ae69478ccfb2e8fa3f5ac9f36a279a6e69f668
Imam Ali Mustofa <d.darkterminal@gmail.com>
2025-06-04 01:25:12 +0700

    build: move migration and clear command in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,13 @@ if [ ! -f /var/www/html/database/database.sqlite ] && [ ! -f /var/www/html/datab
 fi
 
 if [ "$APP_ENV" = "production" ]; then
+    php artisan migrate:fresh --seed --force
     php artisan sqld:remove-database-except-default
+    php artisan optimize
+    php artisan cache:clear
+    php artisan config:clear
+    php artisan route:clear
+    php artisan view:clear
     exec php artisan octane:start --server=frankenphp --host=0.0.0.0 --port=8000
 else
     exec composer run dev
