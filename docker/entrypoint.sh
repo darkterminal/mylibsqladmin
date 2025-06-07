13bcd2fc2189b8397f59f05a9b30f8a38d3b7887
Imam Ali Mustofa <d.darkterminal@gmail.com>
2025-06-07 10:45:14 +0700

    build: generate key using openssl

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,9 +4,10 @@ if [ ! -f /var/www/html/database/database.sqlite ] && [ ! -f /var/www/html/datab
     touch /var/www/html/database/database.sqlite
 fi
 
+APP_KEY="base64:$(openssl rand -base64 32)"
+
 echo "🚀 Starting $APP_ENV server..."
 if [ "$APP_ENV" = "production" ]; then
-    php artisan key:generate --force
     exec php artisan octane:start --server=frankenphp --host=0.0.0.0 --port=8000
 else
     exec composer run dev
